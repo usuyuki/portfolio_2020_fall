837f41d31cfc28a3385687c0124b1eb3b97a86e3
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-11-02 23:37:12 +0900

    fix 文字数変更

--- function.php
+++
@@ -1,6 +0,0 @@
-<!-- googlemapを使おうとしたが費用かかるから断念した -->
-function my_acf_google_map_api( $api ){
-$api['key'] = 'AIzaSyDi7hWAcr5OCExpwjwsemDiKY7K301HNHU';
-return $api;
-}
-add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* WordPress標準機能
+*
+* @codex
+https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/add_theme_support
+*/
+function my_setup() {
+add_theme_support( 'post-thumbnails' ); /* アイキャッチ */
+add_theme_support( 'automatic-feed-links' ); /* RSSフィード */
+add_theme_support( 'title-tag' ); /* タイトルタグ自動生成 */
+add_theme_support( 'html5', array( /* HTML5のタグで出力 */
+'search-form',
+'comment-form',
+'comment-list',
+'gallery',
+'caption',
+) );
+}
+add_action( 'after_setup_theme', 'my_setup' );
+
+
+function custom_excerpt_length( $length ) {
+return 200; //表示したい文字数
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+
+
+
+function new_excerpt_more($more) {
+$html = '<a href="' . esc_url( get_permalink() ) . '">[...続きを読む]</a>';
+return $html;
+}
+
+add_filter('excerpt_more', 'new_excerpt_more');

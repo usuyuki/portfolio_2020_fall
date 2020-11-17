4612d39875d355fbff761df877adab0ddc764cf5
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-11-17 09:59:33 +0900

    feat 418エラー

---
+++ 418.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<!-- ここからメインコンテンツ -->
+<div class="col-md-10" style="min-height:100vh;">
+
+    <div class="ekimeihyou text-center mt-3 mb-5">
+        <!-- <p class="h1 mx-auto text-center">Hello World</p> -->
+        <img src="<?php echo get_template_directory_uri(); ?>/img/bg/coffee_bg.jpg">
+
+    </div>
+    <div class="row text-center text-white">
+        <p class="h1 m-3  mx-auto">418 I'm a teapot!</p>
+        <p class="h5 m-4  mx-auto">指定されたページは存在しません。</p>
+    </div>
+
+
+
+</div><!-- メインのcolのdiv閉じ -->
+
+
+
+<?php get_sidebar(); ?>
+
+
+
+<?php get_footer(); ?>

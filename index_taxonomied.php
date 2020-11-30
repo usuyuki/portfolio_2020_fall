30a883651a2875537e625d5dffa25b649a58e9b5
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-11-30 11:50:12 +0900

    feat cssアニメーションによるindexページの改善

--- index_taxonomied.php
+++ index_taxonomied.php
@@ -32,170 +32,174 @@
         <div class="row text-center pt-5 indexFont">
             <p class="h1 m-3  mx-auto ">Skills</p>
         </div>
-        <div class="row text-center d-flex justify-content-around flex-wrap">
+        <label for="label1">クリックして表示</label>
+        <input type="checkbox" id="label1" />
+        <div class="hidden_show">
+            <div class="row text-center d-flex justify-content-around flex-wrap">
 
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-html5 fa-4x"></i><br>
-                    HTML
-                </p>
-                <p>そこそこ</p>
-            </div>
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-css3-alt fa-4x"></i><br>
-                    CSS
-                </p>
-                <p>そこそこ</p>
-            </div>
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-js fa-4x"></i><br>
-                    JavaScript
-                </p>
-                <p>まだまだ</p>
-            </div>
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-php fa-4x"></i><br>
-                    PHP
-                </p>
-                <p>びみょう</p>
-            </div>
-            <div class="  p-3 rounded skill my-2">
-                <p>
-                    <i class="fab fa-laravel fa-4x mx-1"></i><br>
-                    Laravel
-                </p>
-                <p>そこそこ</p>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-html5 fa-4x"></i><br>
+                        HTML
+                    </p>
+                    <p>そこそこ</p>
+                </div>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-css3-alt fa-4x"></i><br>
+                        CSS
+                    </p>
+                    <p>そこそこ</p>
+                </div>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-js fa-4x"></i><br>
+                        JavaScript
+                    </p>
+                    <p>まだまだ</p>
+                </div>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-php fa-4x"></i><br>
+                        PHP
+                    </p>
+                    <p>びみょう</p>
+                </div>
+                <div class="  p-3 rounded skill my-2">
+                    <p>
+                        <i class="fab fa-laravel fa-4x mx-1"></i><br>
+                        Laravel
+                    </p>
+                    <p>そこそこ</p>
+                </div>
+
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-react fa-4x"></i><br>
+                        React
+                    </p>
+                    <p>まだまだ</p>
+                </div>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-python fa-4x"></i><br>
+                        Python
+                    </p>
+                    <p>そこそこ</p>
+                </div>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-codepen fa-4x"></i><br>
+                        C言語
+                    </p>
+                    <p>まだまだ</p>
+                </div>
+
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-bootstrap fa-4x"></i><br>
+                        Bootstrap
+                    </p>
+                    <p>そこそこ</p>
+                </div>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-git-alt fa-4x"></i><br>
+                        Git
+                    </p>
+                    <p>そこそこ<span style="font-size:0.5em">(GUI操作ならOK)</span></p>
+                </div>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fab fa-wordpress fa-4x"></i><br>
+                        WordPress
+                    </p>
+                    <p>びみょう</p>
+                </div>
+                <div class="  p-3 rounded skill my-2 mx-1">
+                    <p>
+                        <i class="fas fa-microchip fa-4x"></i><br>
+                        Arduino
+                    </p>
+                    <p>びみょう</p>
+                </div>
             </div>
 
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-react fa-4x"></i><br>
-                    React
-                </p>
-                <p>まだまだ</p>
+            <!-- 環境 -->
+            <div class="row text-center pt-5 indexFont">
+                <p class="h1 m-3  mx-auto ">Environment</p>
             </div>
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-python fa-4x"></i><br>
-                    Python
-                </p>
-                <p>そこそこ</p>
-            </div>
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-codepen fa-4x"></i><br>
-                    C言語
-                </p>
-                <p>まだまだ</p>
-            </div>
+            <div class="row text-center d-flex justify-content-around flex-wrap">
 
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-bootstrap fa-4x"></i><br>
-                    Bootstrap
-                </p>
-                <p>そこそこ</p>
+                <div class="environment  p-3 rounded  my-2 mx-1">
+                    <p>
+                        <i class="fas fa-file-code fa-3x"></i><br>
+                        VScode
+                    </p>
+                    <p>エディタ</p>
+                </div>
+                <div class="environment  p-3 rounded my-2 mx-1">
+                    <p>
+                        <i class="fab fa-github fa-3x"></i><br>
+                        GitHub
+                    </p>
+                    <p>バージョン管理</p>
+                </div>
+                <div class="environment p-3 rounded  my-2 mx-1">
+                    <p>
+                        <i class="fab fa-figma fa-3x"></i><br>
+                        Figma
+                    </p>
+                    <p>webサイトデザイン</p>
+                </div>
+                <div class="environment  p-3 rounded my-2 mx-1">
+                    <p>
+                        <i class="fas fa-server fa-3x"></i><br>
+                        SakuraVPS
+                    </p>
+                    <p>VPSサーバー</p>
+                </div>
+                <div class="environment  p-3 rounded my-2 mx-1">
+                    <p>
+                        <i class="fab fa-centos fa-3x"></i><br>
+                        CentOS(Kusanagi)
+                    </p>
+                    <p>サーバーOS<span style="font-size:0.5em">(学習中)</span></p>
+                </div>
+                <div class="environment  p-3 rounded my-2 mx-1">
+                    <p>
+                        <i class="fab fa-windows fa-3x"></i><br>
+                        Windows10
+                    </p>
+                    <p>ローカルOS</p>
+                </div>
+                <div class="environment  p-3 rounded my-2 mx-1">
+                    <p>
+                        <i class="fab fa-docker fa-3x"></i><br>
+                        Docker
+                    </p>
+                    <p>開発環境<span style="font-size:0.5em">(学習中)</span></p>
+                </div>
+                <div class="environment  p-3 rounded my-2 mx-1">
+                    <p>
+                        <i class="fas fa-mug-hot fa-3x"></i><br>
+                        Hot Coffee
+                    </p>
+                    <p>ブースト環境</p>
+                </div>
             </div>
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-git-alt fa-4x"></i><br>
-                    Git
-                </p>
-                <p>そこそこ<span style="font-size:0.5em">(GUI操作ならOK)</span></p>
-            </div>
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fab fa-wordpress fa-4x"></i><br>
-                    WordPress
-                </p>
-                <p>びみょう</p>
-            </div>
-            <div class="  p-3 rounded skill my-2 mx-1">
-                <p>
-                    <i class="fas fa-microchip fa-4x"></i><br>
-                    Arduino
-                </p>
-                <p>びみょう</p>
-            </div>
-        </div>
-        <!-- 環境 -->
-        <div class="row text-center pt-5 indexFont">
-            <p class="h1 m-3  mx-auto ">Environment</p>
-        </div>
-        <div class="row text-center d-flex justify-content-around flex-wrap">
-
-            <div class="environment  p-3 rounded  my-2 mx-1">
-                <p>
-                    <i class="fas fa-file-code fa-3x"></i><br>
-                    VScode
-                </p>
-                <p>エディタ</p>
-            </div>
-            <div class="environment  p-3 rounded my-2 mx-1">
-                <p>
-                    <i class="fab fa-github fa-3x"></i><br>
-                    GitHub
-                </p>
-                <p>バージョン管理</p>
-            </div>
-            <div class="environment p-3 rounded  my-2 mx-1">
-                <p>
-                    <i class="fab fa-figma fa-3x"></i><br>
-                    Figma
-                </p>
-                <p>webサイトデザイン</p>
-            </div>
-            <div class="environment  p-3 rounded my-2 mx-1">
-                <p>
-                    <i class="fas fa-server fa-3x"></i><br>
-                    SakuraVPS
-                </p>
-                <p>VPSサーバー</p>
-            </div>
-            <div class="environment  p-3 rounded my-2 mx-1">
-                <p>
-                    <i class="fab fa-centos fa-3x"></i><br>
-                    CentOS(Kusanagi)
-                </p>
-                <p>サーバーOS<span style="font-size:0.5em">(学習中)</span></p>
-            </div>
-            <div class="environment  p-3 rounded my-2 mx-1">
-                <p>
-                    <i class="fab fa-windows fa-3x"></i><br>
-                    Windows10
-                </p>
-                <p>ローカルOS</p>
-            </div>
-            <div class="environment  p-3 rounded my-2 mx-1">
-                <p>
-                    <i class="fab fa-docker fa-3x"></i><br>
-                    Docker
-                </p>
-                <p>開発環境<span style="font-size:0.5em">(学習中)</span></p>
-            </div>
-            <div class="environment  p-3 rounded my-2 mx-1">
-                <p>
-                    <i class="fas fa-mug-hot fa-3x"></i><br>
-                    Hot Coffee
-                </p>
-                <p>ブースト環境</p>
-            </div>
-        </div>
-        <!-- Git-grass -->
-        <!-- <div class="mt-5 mb-3 d-none d-md-block" style="flex-basis:20%;">
+            <!-- Git-grass -->
+            <!-- <div class="mt-5 mb-3 d-none d-md-block" style="flex-basis:20%;">
             <img src="https://grass-graph.moshimo.works/images/Usuyuki.png">
 
         </div> -->
-        <!-- 最近の制作物 -->
-        <hr>
-        <div class="row text-center pt-5 indexFont">
-            <p class="h1 mt-3 mb-0 mx-auto ">Latest Works</p>
-        </div>
-        <div class="">
-            <?php $args=array(
+            <!-- 最近の制作物 -->
+            <hr>
+            <div class="row text-center pt-5 indexFont">
+                <p class="h1 mt-3 mb-0 mx-auto ">Latest Works</p>
+            </div>
+            <div class="">
+                <?php $args=array(
             'numberposts'=>2,
             'post_type'=>'work',
             'orderby' => 'meta_value',
@@ -216,13 +220,13 @@
                 foreach($customPost as $post):
                     // setup_postdata($post):
             ?>
-            <div class=" border mx-5 my-4 p-1 rounded row " style="min-width:70%">
+                <div class=" border mx-5 my-4 p-1 rounded row " style="min-width:70%">
 
-                <!-- 画像 -->
-                <div class="col-12 col-md-4">
-                    <div class="work-img m-2">
-                        <a href="<?php echo get_permalink(); ?>">
-                            <?php 
+                    <!-- 画像 -->
+                    <div class="col-12 col-md-4">
+                        <div class="work-img m-2">
+                            <a href="<?php echo get_permalink(); ?>">
+                                <?php 
             $image = get_field('work_img');
             $size = 'thumbnail_medium'; // (thumbnail, medium, large, full or custom size)
             if( $image ) {
@@ -234,65 +238,65 @@
                 
             }
             ?>
-                        </a>
-                    </div>
-
-                </div>
-
-
-
-                <!-- 詳細情報 -->
-                <div class="col-12 col-md-8">
-                    <div class="d-flex flex-column p-2">
-                        <div class="text-decoration-none text-white  mx-auto">
-                            <a class="h4 text-center my-2" href="<?php echo get_permalink(); ?>">
-                                "<?php the_title(); ?>"</a>
+                            </a>
                         </div>
-                        <div class="row mt-1">
 
-                            <div class="col-12 col-md-3 detail my-1 mx-auto my-mt-auto">
-                                <p class="text-warning">公開・デプロイ日:<?php the_field ( "work_start" ); ?></p>
-
-                            </div>
-                            <div class="col-12 col-md-3 detail  my-1 mx-auto my-mt-auto">
-                                <p class="text-info">使用技術:<?php the_field ( "work_tech" ); ?></p>
-
-                            </div>
-                            <div class="col-12 col-md-3 work-kind  my-1 mx-auto my-mt-auto">
-                                <p class="p-2 border border-primary rounded ">
-                                    <?php the_field ( "work_kind" ); ?>
-                                </p>
-                            </div>
-                        </div>
-                        <div class="mt-4 pl-0  text-left mx-center">
-
-                            <p><?php the_excerpt(); ?></p>
-                        </div>
                     </div>
 
 
 
+                    <!-- 詳細情報 -->
+                    <div class="col-12 col-md-8">
+                        <div class="d-flex flex-column p-2">
+                            <div class="text-decoration-none text-white  mx-auto">
+                                <a class="h4 text-center my-2" href="<?php echo get_permalink(); ?>">
+                                    "<?php the_title(); ?>"</a>
+                            </div>
+                            <div class="row mt-1">
+
+                                <div class="col-12 col-md-3 detail my-1 mx-auto my-mt-auto">
+                                    <p class="text-warning">公開・デプロイ日:<?php the_field ( "work_start" ); ?></p>
+
+                                </div>
+                                <div class="col-12 col-md-3 detail  my-1 mx-auto my-mt-auto">
+                                    <p class="text-info">使用技術:<?php the_field ( "work_tech" ); ?></p>
+
+                                </div>
+                                <div class="col-12 col-md-3 work-kind  my-1 mx-auto my-mt-auto">
+                                    <p class="p-2 border border-primary rounded ">
+                                        <?php the_field ( "work_kind" ); ?>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="mt-4 pl-0  text-left mx-center">
+
+                                <p><?php the_excerpt(); ?></p>
+                            </div>
+                        </div>
+
+
+
+                    </div>
                 </div>
+                <?php endforeach; ?>
+                <?php else: ?>
+                <p>作品がありません</p>
+                <?php endif; ?>
+
             </div>
-            <?php endforeach; ?>
-            <?php else: ?>
-            <p>作品がありません</p>
-            <?php endif; ?>
-
+            <!-- ボタン、アニメーションもつけてみた -->
+            <div class="text-center mx-auto index-work-btn d-block p-2 mt-0 mb-4" style="width:40vh">
+                <a href="/work">
+                    <p>制作物全体は<wbr>こちらからご覧になれます</p>
+                </a>
+            </div>
         </div>
-        <!-- ボタン、アニメーションもつけてみた -->
-        <div class="text-center mx-auto index-work-btn d-block p-2 mt-0 mb-4" style="width:40vh">
-            <a href="/work">
-                <p>制作物全体は<wbr>こちらからご覧になれます</p>
-            </a>
-        </div>
-    </div>
-</div><!-- メインのcolのdiv閉じ -->
+    </div><!-- メインのcolのdiv閉じ -->
 
 
 
-<?php get_sidebar(); ?>
+    <?php get_sidebar(); ?>
 
 
 
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
